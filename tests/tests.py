a895a8451ad89a7654eb0c787592187639f3e204
Digirolamo <CDigirolamo@digispade.com>
2017-07-31 22:48:16 -0400

    Added a few tests

---
+++ tests/tests.py
@@ -0,0 +1,72 @@
+"""This module contains the general flownet tests.
+
+"""
+from unittest import TestCase
+from flownet import FlowNetwork
+
+
+def get_max_flow(adj_matrix, sources, sinks):
+    """Gets the maximum flow of the flow network from sources to sinks.
+
+    Args:
+            adjacency_matrix (list[list[int]): The adjacency matrix representation
+                of a flow graph.
+            sources (list[int]): list of integers denoting indexes of sources.
+            sinks (list[int]): list of integers denoting indexes of sinks.
+
+    Returns:
+        int: The maximum flow.
+
+    """
+    flow_network = FlowNetwork.from_adjacency_matrix(adj_matrix, sources, sinks)
+    return flow_network.get_maximum_flow()
+
+
+class MaxFlowTestCases(TestCase):
+    def test_max1(self):
+        sources = [0]
+        sinks = [1]
+        adj_matrix = [
+            [0, 0],
+            [0, 0]
+            ]
+        assert get_max_flow(adj_matrix, sources, sinks) == 0
+
+
+    def test_max2(self):
+        sources = [0]
+        sinks = [1]
+        adj_matrix = [
+            [2, 6],
+            [3, 1]
+            ]
+        assert get_max_flow(adj_matrix, sources, sinks) == 6
+
+    def test_max3(self):
+        sources = [0, 3, 13, 1, 7, 15, 5]
+        sinks = [11, 14, 10, 9, 2, 12, 17, 16, 19],
+        adj_matrix = [
+            [1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1],
+            [1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1],
+            [1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+            [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1],
+            [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+            [1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+            [0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0],
+            [1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+            [0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+            [0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1],
+            [1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0],
+            [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0],
+            [0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0],
+            [1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+            [1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0]
+            ]
+        assert get_max_flow(adj_matrix, sources, sinks) == 53
