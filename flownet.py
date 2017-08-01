096e318e8c794fa544e30e70100d453c47e4a72e
Digirolamo <CDigirolamo@digispade.com>
2017-07-31 22:05:50 -0400

    Added documentation and cleaned up after using pylint.

--- flownet.py
+++ flownet.py
@@ -1,15 +1,14 @@
 """A simple flow network module.
 
 This module contains the classes and functions written to create, examine,
-and solve simple problems on flow networks. 
+and solve simple problems on flow networks.
 
 Copyright (C) 2017 Chris Digirolamo
 
 """
 from collections import deque, OrderedDict
-from copy import deepcopy
 from decimal import Decimal
-from itertools import chain
+import weakref
 
 INF = Decimal('Infinity')
 
@@ -26,7 +25,7 @@ class FlowNode(object):
 
     Attributes:
         key (int): A unique identifier for the node.
-        
+
         edge_flow (dict[FlowNode, int]): Maps the flow for this node.
             Stores the flow of children nodes. Basically stores the
             edge.
@@ -49,7 +48,7 @@ class FlowNode(object):
     def key(self):
         """object: The unique node identifier."""
         return self._key
-    
+
     def add_flow(self, child, amount):
         """
         Adds a a flow from this node to a child node.
@@ -89,46 +88,46 @@ class FlowNode(object):
 
     def iter_children(self):
         """Iterate through the nodes that this node flows into.
-        
+
         Yields:
             FlowNode: Each child of this node.
-            
+
             """
         for child in self.edge_flow.iterkeys():
-            yield child     
-            
+            yield child
+
     def iter_parents(self):
         """Iterate through the nodes that flow into this node.
-        
+
         Yields:
             FlowNode: Each parents of this node.
-            
+
             """
         #Stored as weakrefs
         for ref in self._parents:
             parent = ref()
             if parent is not None:
                 yield parent
-          
+
     def iter_edges(self):
         """Iterate through the edges of this node.
-        
+
         Yields:
             tuple: (parent, child, capacity) of the flow.
-            
+
         """
         for child, capacity in self.edge_flow.iteritems():
             yield (self, child, capacity)
-        
+
     def iter_dfs_edges(self):
         """ A depth first search following the flow from this node.
-        
+
         Note:
             The first node yielded is the calling instance itself.
 
         Yields:
             tuple[FlowNode]: The next (parent, child) nodes in the search.
-        
+
         """
         node_queue = deque([(self, c) for c in self.edge_flow.iterkeys()])
         visited = set([c for p, c in node_queue])
@@ -140,33 +139,33 @@ class FlowNode(object):
                 if granchild not in visited:
                     visited.add(granchild)
                     node_queue.append((child, granchild))
-        
+
     def __repr__(self):
         cls_name = self.__class__.__name__
         text = "{}({})"
         return text.format(cls_name, self.key)
 
-    
+
 class SuperSink(FlowNode):
     """The only sink node.
-    
+
     If there are multiple sinks in a graph, one instance of this node
     can handle them.
-    
+
     Attributes:
         consumed (int): As a sink, flows are consumed, this
             counts the consumed flow.
-    
+
     """
     def __init__(self, *args, **kwargs):
         super(SuperSink, self).__init__(*args, **kwargs)
         self.consumed = 0
-        
+
     @property
     def flow(self):
         """int: The total flow from this node."""
         return INF
-    
+
     def add_flow(self, child, amount):
         """
         Rather than adding flow from this node to a child node,
@@ -181,16 +180,16 @@ class SuperSink(FlowNode):
         """
         self.consumed += amount
 
-        
+
 class SuperSource(FlowNode):
     """The only source node.
-    
+
     If there are multiple sources in a graph, one instance of this node
     can handle them.
-    
+
     """
-    
-    
+
+
 class FlowNetwork(object):
     """
     A class for flow network functionality. This class is primarily
@@ -198,7 +197,8 @@ class FlowNetwork(object):
     class.
 
     Attributes:
-        node_key_dict (OrderedDict[key, FlowNode]): The dict of all created nodes.
+        node_key_dict (OrderedDict[key, FlowNode]): The dict of all created
+            nodes.
         _source (SuperSource): The only source node.
         _sink (SuperSink): The only sink node.
 
@@ -206,37 +206,37 @@ class FlowNetwork(object):
     def __init__(self):
         self._source = SuperSource("+")
         self._sink = SuperSink("-")
-        d = [(self.source.key, self.source), (self.sink.key, self.sink)]
-        self.node_key_dict = OrderedDict(d)
-        
+        start_d = [(self.source.key, self.source), (self.sink.key, self.sink)]
+        self.node_key_dict = OrderedDict(start_d)
+
     @property
     def source(self):
         """SuperSource: The only source node."""
         return self._source
-    
+
     @property
     def sink(self):
         """SuperSink: The only sink node."""
         return self._sink
-    
+
     def add_node(self, node):
         """Adds a node to this graph.
-        
+
         Args:
             node (FlowNode): The node to add.
-        
-        
+
+
         """
         if node is self.source or node is self.sink:
             return
-        elif isinstance(node, SuperSink) or isinstance(node, SuperSource):
+        elif isinstance(node, (SuperSink, SuperSource)):
             raise ValueError("Cannot add SuperSink or SuperSource node : {}"
                              "".format(node))
         if node.key in self.node_key_dict:
             raise KeyError("Node already added: Cannot add {}."
-                          "".format(node))
+                           "".format(node))
         self.node_key_dict[node.key] = node
-        
+
     def add_flow_edge(self, parent, child, capacity):
         """
         Adds a a flow from a parent node to a child node.
@@ -264,20 +264,19 @@ class FlowNetwork(object):
         elif child.key not in self.node_key_dict:
             self.add_node(child)
         parent.add_flow(child, capacity)
-        
+
     def get_maximum_flow(self):
         """Gets the maximum flow from source to sink.
-        
+
         Returns:
             int: The maximum flow.
-            
+
         """
         new_copy = self.from_flow_network(self)
         start_consumed = new_copy.sink.consumed
         new_copy.send_max_flow_to_sink()
         return new_copy.sink.consumed - start_consumed
-        
-        
+
     def send_max_flow_to_sink(self):
         """
         Sends the maximum amount of flow from the source to the sink.
@@ -290,7 +289,7 @@ class FlowNetwork(object):
         how much the sink consumed.
 
         """
-        parent_map = self.get_dfs_sink_flow_path()
+        parent_map = self._get_dfs_sink_flow_path()
         while parent_map:
             minimum_path_flow = INF
             node = self.sink
@@ -306,9 +305,9 @@ class FlowNetwork(object):
                 parent.reduce_flow(node, minimum_path_flow)
                 node.add_flow(parent, minimum_path_flow)
                 node = parent
-            parent_map = self.get_dfs_sink_flow_path()
+            parent_map = self._get_dfs_sink_flow_path()
 
-    def get_dfs_sink_flow_path(self):
+    def _get_dfs_sink_flow_path(self):
         """A single path from this node to the sink.
 
         A breadth first search following the flow from
@@ -320,7 +319,6 @@ class FlowNetwork(object):
 
         """
         node_path = {}
-        sink_found = False
         for parent, child in self.source.iter_dfs_edges():
             node_path[child] = parent
             if child is self.sink:
@@ -338,11 +336,11 @@ class FlowNetwork(object):
 
     def iter_edge_values(self):
         """Iterator of all edges.
-        
+
         Yields:
             tuple[]: (parent, child, value) for all edges in the
             flow network.
-            
+
         """
         visited = set()
         for node in self.node_key_dict.values():
@@ -354,7 +352,7 @@ class FlowNetwork(object):
                     continue
                 visited.add(edge)
                 yield parent, child, parent.edge_flow[child]
-        
+
     @classmethod
     def from_adjacency_matrix(cls, adjacency_matrix, sources, sinks):
         """Creates a Flow Network from an adjacency matrix.
@@ -364,18 +362,18 @@ class FlowNetwork(object):
                 of a flow graph.
             sources (list[int]): list of integers denoting indexes of sources.
             sinks (list[int]): list of integers denoting indexes of sinks.
-        
+
         """
-        flow_graph = cls()
+        flow_network = cls()
         key_to_node = {}
         sources = set(sources)
         sinks = set(sinks)
         size = len(adjacency_matrix)
         for node_idx in xrange(size):
             if node_idx in sinks:
-                node = flow_graph.sink
+                node = flow_network.sink
             elif node_idx in sources:
-                node = flow_graph.source
+                node = flow_network.source
             else:
                 node = FlowNode(node_idx)
             key_to_node[node_idx] = node
@@ -386,20 +384,22 @@ class FlowNetwork(object):
             for next_node_idx in xrange(size):
                 next_node = key_to_node[next_node_idx]
                 capacity = row[next_node_idx]
-                flow_graph.add_flow(node, next_node, capacity)
-        return flow_graph
-          
+                flow_network.add_flow_edge(node, next_node, capacity)
+        return flow_network
+
     @classmethod
     def from_flow_network(cls, flow_network):
-        """
-        
+        """Creates a copy of a flow network.
+
+        Args:
+            flow_network (FlowNetwork): The instance to copy.
+
         """
         new_flow_network = cls()
         for parent, child, capacity in flow_network.iter_edge_values():
-            new_flow_network.add_flow(parent.key, child.key, capacity)
+            new_flow_network.add_flow_edge(parent.key, child.key, capacity)
         return new_flow_network
 
-        
     def __str__(self):
         all_strings = []
         for node in self.node_key_dict.itervalues():
